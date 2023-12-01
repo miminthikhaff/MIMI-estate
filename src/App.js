126593aa2c43c9b9c269f1c2e104a55d92c6cc07
miminthikhaff github <you@example.com>
2023-12-01 18:19:00 +0530

    create header components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import SignIn from './Pages/SignIn';
 import SignOut from './Pages/SignOut';
 import Profile from './Pages/Profile';
 import About from './Pages/About';
+import Header from './Components/Header';
 
 function App() {
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/Sign-In' element={<SignIn />} />
