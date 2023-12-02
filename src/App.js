7098e99697c02baeac0fbd0fa4e960c25c5a489c
miminthikhaff github <you@example.com>
2023-12-02 17:06:49 +0530

    complete sign up page ui

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
-import SignOut from './Pages/SignOut';
+import SignUp from './Pages/SignUp';
 import Profile from './Pages/Profile';
 import About from './Pages/About';
 import Header from './Components/Header';
@@ -13,7 +13,7 @@ function App() {
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/Sign-In' element={<SignIn />} />
-    <Route path='/Sign-Out' element={<SignOut />} />
+    <Route path='/Sign-Up' element={<SignUp />} />
     <Route path='/About' element={<About />} />
     <Route path='/Profile' element={<Profile />} />
     
